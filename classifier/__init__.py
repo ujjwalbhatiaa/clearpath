6570e3e029b84398568e097a0e6af7d57c8b581d
Ujjwal Bhatia <ujjwalbhatia001@gmail.com>
2026-07-16 22:09:07 +0530

    Initialize classifier package with backend imports

---
+++ classifier/__init__.py
@@ -0,0 +1,13 @@
+"""ClearPath sponsorship/eligibility classifier package."""
+
+from .backend import (
+    ClassifierBackend,
+    HeuristicBackend,
+    ClassificationResult,
+)
+
+__all__ = [
+      "ClassifierBackend",
+      "HeuristicBackend",
+      "ClassificationResult",
+]
